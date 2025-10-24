a341135f35efe98264ec3dd0daca484ebbef4b29
Sampo Savolainen <sampo.savolainen@spatineo.com>
2025-10-24 11:53:16 +0000

    working, but boring version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
 
-import * as CrossSection from './assets/example-trajectory.json'
 import { CrossSectionD3 } from './components/CrossSectionD3'
 
 function App() {
   return (
     <>
-      <CrossSectionD3 data={CrossSection} />
+      <CrossSectionD3/>
     </>
   )
 }
--- src/components/CrossSectionD3.tsx
+++ src/components/CrossSectionD3.tsx
@@ -1,14 +1,81 @@
 import * as d3 from "d3";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
+import { uniq } from "lodash";
+import { generateExampleTrajectoryCovJSON } from "../data/exampleData";
+import type { FeatureCollection } from "geojson";
 
-interface CrossSectionD3Props {
-    data: unknown
+interface Value {
+    value: number;
+    [id: string]: number|string;
 }
 
-export const CrossSectionD3 = ({data} : CrossSectionD3Props) => {
+interface CrossSectionData {
+    values: Value[],
+    gridWidth: number,
+    gridHeight: number,
+    grid: number[],
+    featureCollection: FeatureCollection 
+}
+
+export const CrossSectionD3 = () => {
     const ref = useRef(null);
 
-    console.log(data);
+    const dataGrids : CrossSectionData = useMemo(() => {
+        const source = generateExampleTrajectoryCovJSON();
+
+        const parameterName = 'temperature';
+
+        const values : Value[] = source.coverages.flatMap((cov) => {
+            const range = cov.ranges[parameterName]
+            // TODO: instead of `composite`, this should be relative to the axisNames in range
+            const axes = cov.domain.axes.composite;
+
+            return range.values.map((value, idx) => {
+                return {
+                    value,
+                    ...axes.coordinates.reduce((memo, axis, axisIdx) => ({
+                        ...memo,
+                        [axis]: axes.values[idx][axisIdx]
+                    }), {} as { [id: string]: string|number })
+                };
+            }).filter(v => v.value !==null) as Value[];
+        });
+
+        values.sort((a, b) => {
+            if (a.z! < b.z!) return -1;
+            if (a.z! > b.z!) return 1;
+            if (a.x! < b.x!) return -1;
+            if (a.x! > b.x!) return 1;
+            return 0;
+        });
+
+        const width = uniq(values.map(v => v.x)).length;
+        const height = uniq(values.map(v => v.z)).length;
+
+        const grid = values.map(v => v.value);
+
+        const polygons = d3.contours().size([width, height])(grid);
+
+        return {
+            values,
+            gridWidth: width,
+            gridHeight: height,
+            grid,
+            featureCollection: {
+                type: "FeatureCollection",
+                features: polygons.map(poly => ({
+                    type: "Feature",
+                    geometry: poly,
+                    properties: {
+                        value: poly.value
+                    }
+                }))
+            }
+        };
+    }, []);
+
+    console.log(dataGrids);
+
 
     useEffect(() => {
         if (!ref.current) return;
@@ -17,7 +84,7 @@ export const CrossSectionD3 = ({data} : CrossSectionD3Props) => {
         width = 460 - margin.left - margin.right,
         height = 400 - margin.top - margin.bottom;
 
-        // append the svg object to the body of the page
+        // TODO: we need to replace the previous svg, not just add a new one in
         const svg = d3
             .select(ref.current)
             .append("svg")
@@ -25,42 +92,21 @@ export const CrossSectionD3 = ({data} : CrossSectionD3Props) => {
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        // Parse the Data
-
-        d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv"
-            ).then(function (data) {
-            // X axis
-            const x = d3
-                .scaleBand()
-                .range([0, width])
-                .domain(data.map((d) => d.Country))
-                .padding(0.2);
-            svg
-                .append("g")
-                .attr("transform", `translate(0, ${height})`)
-                .call(d3.axisBottom(x))
-                .selectAll("text")
-                .attr("transform", "translate(-10,0)rotate(-45)")
-                .style("text-anchor", "end");
-
-            // Add Y axis
-            const y = d3.scaleLinear().domain([0, 13000]).range([height, 0]);
-            svg.append("g").call(d3.axisLeft(y));
-
-            // Bars
-            svg
-                .selectAll("mybar")
-                .data(data)
-                .join("rect")
-                .attr("x", (d) => x(d.Country))
-                .attr("y", (d) => y(d.Value))
-                .attr("width", x.bandwidth())
-                .attr("height", (d) => height - y(d.Value))
-                .attr("fill", "#5f0f40");
-            });
         
-    }, [ref]);
+        const myColor = d3.scaleSequential()
+            .interpolator(d3.interpolateBlues)
+            .domain([d3.min(dataGrids.grid)!, d3.max(dataGrids.grid)!])
+
+        dataGrids.featureCollection.features.forEach(feature => {
+            svg.append("path")
+                .datum(feature)
+                .style("stroke", "none")
+                .style("fill", (x) => myColor(x.properties!.value))
+                .attr("transform", `scale(${width/dataGrids.gridWidth} ${height/dataGrids.gridHeight})`)
+                .attr("d", d3.geoPath())
+        })
+
+    }, [ref, dataGrids]);
 
    return <svg width={460} height={400} id="barchart" ref={ref} />;
 };
